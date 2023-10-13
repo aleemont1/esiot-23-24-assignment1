65ea76c56fd30b81ab467835144d4ce8d1302b79
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-13 16:38:38 +0200

    [GAME_STARTER] Create basic init_game function

--- main/game_starter.cpp
+++ main/game_starter.cpp
@@ -2,7 +2,14 @@
 #include "Arduino.h"
 #include "constants.h"
 #include "fading_red.h"
+#include "led_manager.h"
 
+void init_game() {
+    init_board();
+    reset_board();
+    reset_pulse();
+    Serial.println("Welcome to the Catch the Led Pattern Game. Press Key T1 to Start");
+}
 
 void initial_state() {
     pulse();
