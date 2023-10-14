794e6a59d6cb2697af61b3152fcf2d58463eb287
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-14 16:37:55 +0200

    [BTNS] Init buttons

---
+++ main/buttons_manager.cpp
@@ -0,0 +1,14 @@
+//Buttons manager, in this module all implementations of buttons_manager.h
+#include "buttons_manager.h"
+#include "Arduino.h"
+#include "constants.h"
+
+const uint8_t BTNS = {B1,B2,B3,B4};
+
+void init_buttons() {
+    for(int i = 0; i < N_LED; i++) {
+        pinMode(BTNS[i], INPUT);
+    }
+}
+
+
