d02df20511be8a374a44557fb7bd24a04af1a974
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-14 16:51:51 +0200

    [GAME_MAN] Create macros to define game states

--- main/game_starter.h
+++ main/game_starter.h
@@ -1,3 +1,12 @@
 //Inizializzazione della partita
+
+//Game states
+#define INIT_GAME 0
+#define INITIAL_STATE 1
+#define WAITING_FOR_START_STATE 2
+
+
 void init_game();
 void initial_state();
+void waiting_for_start_state();
+void switch_game_state(const int STATE);
