210dd58047d76c361bfbb2e95606f4393e74b958
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-14 17:34:05 +0200

    [MAIN] Basic initialization of game

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,8 @@
+//#define __DEBUG  //Enable for advanced logging of time and game state.
+
 #include "constants.h"
 #include "game_starter.h"
+#include "buttons_manager.h"
 
 int buttons[4] =  {B1,B2,B3,B4};
 
@@ -8,14 +11,32 @@ unsigned long T1 = random(MIN_DELAY,MAX_DELAY); //Delay random inizio partita
 unsigned long T2 = 250;     //Delay spegnimento singolo LED
 unsigned long T3 = N_LED * T2;  //Delay massimo per partita
 
+extern long elapsed_time_in_state;
+extern int game_state;
+
 void setup() {
   Serial.begin(9600);
-  init_game();
-  for(int i = 0; i < N_LED; i++) {
-    pinMode(buttons[i],INPUT);
-  }
 }
 
 void loop() {
-  initial_state();
+  update_time();
+  #ifdef __DEBUG
+    Serial.print("Current time: ");
+    Serial.print(elapsed_time_in_state);
+    Serial.print(" Current state: ");
+    Serial.println(game_state);
+    Serial.println("\n\n");
+  #endif
+  switch(game_state) {
+    case INIT_GAME:
+      init_game();
+      init_buttons();
+      break;
+    case INITIAL_STATE:
+      initial_state();
+      break;
+    default:
+      Serial.println("Illegal state");
+      break;
+  }
 }
