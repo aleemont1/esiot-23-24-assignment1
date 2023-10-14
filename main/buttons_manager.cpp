0547c66c09bc56d31d31ecbbb1f85c951572a473
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-14 16:50:21 +0200

    [BTNS] Minor data type fixes

--- main/buttons_manager.cpp
+++ main/buttons_manager.cpp
@@ -3,7 +3,7 @@
 #include "Arduino.h"
 #include "constants.h"
 
-const uint8_t BTNS = {B1,B2,B3,B4};
+const uint8_t BTNS[] = {B1,B2,B3,B4};
 
 void init_buttons() {
     for(int i = 0; i < N_LED; i++) {
