e7dc73074b971e1d69a6718b46a7c5d3874b21cf
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-15 16:15:26 +0200

    [GAME_MAN] Create SLEEPING_STATE macro

--- main/game_starter.h
+++ main/game_starter.h
@@ -4,7 +4,7 @@
 #define INIT_GAME 0
 #define INITIAL_STATE 1
 #define WAITING_FOR_START_STATE 2
-
+#define SLEEPING_STATE 3 //Verrà modificato il valore, vorrei che fosse lo stato con valore più alto
 
 void init_game();
 void initial_state();
