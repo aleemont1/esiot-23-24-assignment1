3609c8b571f48bbed14ac681134fce497784a606
davidedima00 <davidedima00@users.noreply.github.com>
2023-10-19 22:38:43 +0200

    [POT] enable pot solve some problems

--- main/buttons_manager.h
+++ main/buttons_manager.h
@@ -2,5 +2,15 @@
 void init_buttons();
 void start_game();
 void button_handler(const int B);
+void readButtonsStatus();
 void interrupt_setup();
+void waitForNormalClick();
 void wait_for_wake_up();
+
+
+/*PROVE*/
+
+void pressInterrupt();
+void configureCommon();
+void configureDistinct();
+void press(int button);
--- main/game_starter.h
+++ main/game_starter.h
@@ -5,15 +5,20 @@
 #define INITIAL_STATE 1
 #define WAITING_FOR_START_STATE 2
 #define GAME_STARTED_STATE 3
+#define INGAME_STATE 5
 #define SLEEPING_STATE 4 // Verrà modificato il valore, vorrei che fosse lo stato con valore più alto
 
 void init_game();
 void initial_state();
 void game_started_state();
+void in_game_state();
 void sleeping_state();
 void switch_game_state(const int STATE);
 void update_time();
 void sleepArduino();
+void setDifficulty(const int value);
+void readPotValue();
+
 #ifdef __DEBUG
 void test();
 #endif
