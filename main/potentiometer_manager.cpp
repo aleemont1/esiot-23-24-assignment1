82bb6dbca0a75b646dccf11ea8e7a05d508b29b4
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-10-22 22:39:05 +0200

    [POT] Fix typos and remove unused variables. Made F extern.

--- main/potentiometer_manager.cpp
+++ main/potentiometer_manager.cpp
@@ -2,27 +2,27 @@
 #include "Arduino.h"
 #include "constants.h"
 
-int F; //moltiplicatore velocità di gioco
+extern double F; //moltiplicatore velocità di gioco
 int difficulty;
-int newDifficult;
-int previousDifficult;
 
 void setDifficulty(const int difficulty)
 {
     Serial.begin(9600);
-    F = 1.0 + difficulty * 0.1;
-    Serial.println("Difficulty: ");
+    F = 1.0 + difficulty * 0.1;  
+    Serial.print("F: ");
+    Serial.print(F);
+    Serial.println(" Difficulty: ");
     Serial.println(difficulty);
     Serial.end();
 }
 
 void readPotValue()
 {
-    int newDifficult = map(analogRead(POT), 0, 1023, 1, MAX_DIFFICULT);
-    delay(1);
-    if (newDifficult != previousDifficult)
+    int newDifficulty = map(analogRead(POT), 0, 1023, 1, MAX_DIFFICULT);
+    delay(10);
+    if (newDifficulty != difficulty)
     {
-        setDifficulty(newDifficult);
-        previousDifficult = newDifficult;
+        setDifficulty(newDifficulty);
+        difficulty = newDifficulty;
     }
 }
